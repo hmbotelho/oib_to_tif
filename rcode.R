2eb6fef62e7abb4804e0d0b7b846a1e423fc0831
unknown <hugobotelho@gmail.com>
2022-05-16 11:44:32 +0100

    try conversion to ome.tiff

--- rcode.R
+++ rcode.R
@@ -7,11 +7,11 @@ img = read.image(fname_oib, normalize = FALSE)[,,2]
 
 
 # Get image metadata
-pixelWidth <- as.numeric(img@metadata$globalMetadata$`[Reference Image Parameter] WidthConvertValue`)
-pixelHeight <- as.numeric(img@metadata$globalMetadata$`[Reference Image Parameter] HeightConvertValue`)
-pixelsizeunit <- img@metadata$globalMetadata$`[Reference Image Parameter] WidthUnit`
+# pixelWidth <- as.numeric(img@metadata$globalMetadata$`[Reference Image Parameter] WidthConvertValue`)
+# pixelHeight <- as.numeric(img@metadata$globalMetadata$`[Reference Image Parameter] HeightConvertValue`)
+# pixelsizeunit <- img@metadata$globalMetadata$`[Reference Image Parameter] WidthUnit`
 
 
-# save as tif
-fname_tif = sub(".oib", ".tif", fname_oib)
-write.image(img, fname_tif, force = TRUE, pixelType = "uint16")     # pixel size metadata is lost...
+# save as ome.tif
+fname_tif = sub(".oib", ".ome.tiff", fname_oib)
+write.image(img, fname_tif, force = TRUE, pixelType = "uint16")
